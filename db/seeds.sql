50f37edd1510bb2ee8799f43a80a4dbbf4abba12
Siyi Guo <guosiyi1993@gmail.com>
2018-08-18 21:51:40 -0400

    final draft, css needed to be fixed' '

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('avocado', false);
+INSERT INTO burgers (burger_name, devoured) VALUEs ('patato', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('big mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('big daddy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese Burger', false);
